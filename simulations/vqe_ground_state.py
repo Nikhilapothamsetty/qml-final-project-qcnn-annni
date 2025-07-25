8ce6598c389c07e1d1225f4a37a2eed46901b0d8
Nikhilapothamsetty <nikhilapothamsetty@gmail.com>
2025-07-25 17:59:46 +0530

    Update vqe_ground_state.py

--- simulations/vqe_ground_state.py
+++ simulations/vqe_ground_state.py
@@ -1,56 +1,106 @@
-from qiskit import Aer
-from qiskit.utils import QuantumInstance
-from qiskit.opflow import I, X, Z
-from qiskit.circuit.library import EfficientSU2
-from qiskit.algorithms import VQE
-from qiskit.algorithms.optimizers import COBYLA
+from qiskit import QuantumCircuit, Aer, execute
+from qiskit.algorithms.optimizers import SPSA
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-def construct_annni_hamiltonian(n_qubits, kappa=0.5, h=1.0):
-    terms = []
-    for i in range(n_qubits - 1):
-        op = [I] * n_qubits
-        op[i] = Z
-        op[i + 1] = Z
-        term = op[0]
-        for j in range(1, n_qubits):
-            term = term ^ op[j]
-        terms.append(term)
+def generate_data(n_samples=60):
+    data = []
+    labels = []
+    for i in range(n_samples):
+        label = i % 3
+        qc = QuantumCircuit(3)
+        if label == 0:
+            qc.ry(np.pi/4, 0)
+        elif label == 1:
+            qc.ry(np.pi/2, 1)
+            qc.cx(1, 2)
+        else:
+            qc.ry(3*np.pi/4, 0)
+            qc.cx(0, 1)
+            qc.cx(1, 2)
+        data.append(qc)
+        labels.append(label)
+    return data, np.array(labels)
 
-    for i in range(n_qubits - 2):
-        op = [I] * n_qubits
-        op[i] = Z
-        op[i + 2] = Z
-        term = op[0]
-        for j in range(1, n_qubits):
-            term = term ^ op[j]
-        terms.append(kappa * term)
+def create_qcnn(params):
+    qc = QuantumCircuit(3)
+    for i in range(3):
+        qc.ry(params[i], i)
+    qc.cx(0, 1)
+    qc.cx(1, 2)
+    for i in range(3):
+        qc.ry(params[3 + i], i)
+    qc.cx(0, 1)
+    qc.cx(1, 2)
+    for i in range(3):
+        qc.ry(params[6 + i], i)
+    qc.cx(0, 1)
+    qc.ry(params[9], 0)
+    qc.measure_all()
+    return qc
 
-    for i in range(n_qubits):
-        op = [I] * n_qubits
-        op[i] = X
-        term = op[0]
-        for j in range(1, n_qubits):
-            term = term ^ op[j]
-        terms.append(h * term)
+def predict(qc_template, params):
+    backend = Aer.get_backend('qasm_simulator')
+    result = execute(qc_template, backend=backend, shots=1024).result()
+    counts = result.get_counts()
+    prediction = max(counts, key=counts.get)
+    return prediction
 
-    return sum(terms)
+def objective(params, X, y_true):
+    y_pred = []
+    for i, qc_data in enumerate(X):
+        full_circuit = qc_data.compose(create_qcnn(params))
+        full_circuit.measure_all()
+        backend = Aer.get_backend('qasm_simulator')
+        result = execute(full_circuit, backend=backend, shots=1024).result()
+        counts = result.get_counts()
+        guess = max(counts, key=counts.get)
+        pred_class = int(guess[-1])
+        y_pred.append(pred_class)
+    return 1 - accuracy_score(y_true, y_pred)
 
-def main():
-    n = 6
-    depth = 3
-    kappa = 0.5
-    h = 1.0
+X, y = generate_data(60)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-    hamiltonian = construct_annni_hamiltonian(n, kappa, h)
-    ansatz = EfficientSU2(n, entanglement='linear', reps=depth)
-    backend = Aer.get_backend("aer_simulator_statevector")
-    qinst = QuantumInstance(backend, seed_simulator=123, seed_transpiler=123, shots=1024)
-    optimizer = COBYLA(maxiter=250)
-    vqe = VQE(ansatz=ansatz, optimizer=optimizer, quantum_instance=qinst)
-    result = vqe.compute_minimum_eigenvalue(operator=hamiltonian)
-    energy = result.eigenvalue.real
-    print("Estimated ground state energy:", round(energy, 6))
+params = 2 * np.pi * np.random.rand(10)
+optimizer = SPSA(maxiter=20)
+opt_params, _, _ = optimizer.optimize(
+    num_vars=len(params),
+    objective_function=lambda p: objective(p, X_train, y_train),
+    initial_point=params
+)
 
-if __name__ == "__main__":
-    main()
+def evaluate(X, y, params):
+    y_pred = []
+    for qc in X:
+        full_circuit = qc.compose(create_qcnn(params))
+        full_circuit.measure_all()
+        result = execute(full_circuit, Aer.get_backend('qasm_simulator'), shots=1024).result()
+        counts = result.get_counts()
+        guess = max(counts, key=counts.get)
+        pred_class = int(guess[-1])
+        y_pred.append(pred_class)
+    return accuracy_score(y, y_pred)
+
+train_acc = evaluate(X_train, y_train, opt_params)
+test_acc = evaluate(X_test, y_test, opt_params)
+
+print(f"{train_acc * 100:.2f}")
+print(f"{test_acc * 100:.2f}")
+
+accuracies = [evaluate(X_train, y_train, params)]
+for i in range(20):
+    new_params = params + 0.1 * np.random.randn(10)
+    acc = evaluate(X_train, y_train, new_params)
+    accuracies.append(acc)
+
+plt.plot(accuracies)
+plt.title("QCNN Accuracy Over Random Trials")
+plt.xlabel("Trial")
+plt.ylabel("Accuracy")
+plt.grid()
+plt.show()
+
 
